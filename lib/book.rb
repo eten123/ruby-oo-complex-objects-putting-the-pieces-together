8cba89ab49f50863b2b01f33658e7afbe9a11550
Eten <eten.uket@yale.edu>
2021-06-19 13:35:06 -0400

    Finished

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,46 @@
+class Book
+
+    def initialize(title) #initalize is a unique method we call to set initial values 
+        @title = title
+    end
+
+    def title  # getter method because it retrieves value of title from the initialize method
+        @title
+    end
+
+    def author=(author) #setter method establishes or reestablishes value of the instance variable
+    @author = author
+    end
+
+    def author  #getter method for author
+        @author
+    end 
+
+    def page_count=(num) #page count needs a setter method which we set equal to the 
+                        #instance variable num
+        @page_count = num
+    end
+
+    def page_count
+        @page_count
+    end
+
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+ 
+end
+
+
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,56 @@
 # Make your shoe class here!
+require "pry"
+class Shoe
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def brand #we dont need to write like a setter below this getter method since 
+        @brand  #we set the value in initalize. Also, we don't add color and whatever
+    end         #else because the program only wants us to initalize the object with 
+                # brand
+
+    def color  
+        @color
+    end
+    
+    def color=(color)
+        @color = color
+    end
+
+    def size  
+        @size
+    end
+    
+    def size=(size)
+        @size = size
+    end
+    def material 
+        @material
+    end
+    
+    def material=(material)
+        @material = material
+    end
+
+    def material 
+        @material
+    end
+    
+    def condition=(condition)
+        @condition = condition
+    end
+    
+    def condition
+        @condition
+    end
+
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new" #updated the conditon instance variable to equal 
+                        # new by setting it equal to the string new
+    end
+
+    
+end
